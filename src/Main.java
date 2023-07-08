7e8b6d99e92377d73e94cdd9359693bdc593a224
arguler <arzaguler@gmail.com>
2023-07-08 21:10:15 +0300

    i have added sun but it has some errors to be resolved

--- src/Main.java
+++ src/Main.java
@@ -2,10 +2,16 @@ public class Main {
 
     public static void main(String[] args) {
 
-solarSystemBasic Omnax=new solarSystemBasic();
-Omnax.Moon="2";
-Omnax.Sun="1";
-Omnax.Planets="2";
-Omnax.Star="4";
+        solarSystemBasic Omnax = new solarSystemBasic();
+        Omnax.Moon = "2";
+        Omnax.Sun = "1";
+        Omnax.Planets = "2";
+        Omnax.Star = "4";
+
+
+        FeatureSun AdamX = new FeatureSun();
+        AdamX.name = "poya";
+        AdamX.temp = 12344231 celcius;
+        AdamX.size = "32942Km";
     }
 }
